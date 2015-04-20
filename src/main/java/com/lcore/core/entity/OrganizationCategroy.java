64f3ea1db83010e91671123f60a4e6db38b50747
LCore <kiritor@hotmail.com>
2015-04-20 11:30:37 +0800

    组织分类实体

---
+++ src/main/java/com/lcore/core/entity/OrganizationCategroy.java
@@ -0,0 +1,31 @@
+package com.lcore.core.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Organization_categroy")
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
+public class OrganizationCategroy {
+	private String name;
+	private Boolean isEnable;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Boolean getIsEnable() {
+		return isEnable;
+	}
+
+	public void setIsEnable(Boolean isEnable) {
+		this.isEnable = isEnable;
+	}
+
+}
